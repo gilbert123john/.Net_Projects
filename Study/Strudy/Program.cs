8b9e013fe0f3f2e1544b5c3a3eb771bf6e50b4de
gilbert123john <johngilbert.kochery@gmail.com>
2023-08-14 10:40:12 +0530

    updated code

---
+++ Study/Strudy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace study
+{
+    public delegate void addNum(int a, int b);
+    public delegate void subNum(int a, int b);
+
+    public class Program
+    {
+        class delClass
+        {
+            public void sum(int a , int b)
+            {
+                Console.WriteLine("Sum is {0}",a+b);
+            }
+            public void sub(int a, int b)
+            {
+                Console.WriteLine("Diff is {0}",a-b);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            delClass obj = new delClass();
+
+            addNum addObj = new addNum(obj.sum);
+            subNum subObj = new subNum(obj.sub);
+            addObj(10, 20);
+            subObj(40, 20);
+        }
+    }
+}
